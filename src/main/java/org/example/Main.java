cdcfa266e21c112e7135fc1af401fd2ea1958ee9
Дроздов Алексей <jokend1ng2@yandex.ru>
2023-05-08 22:12:48 +0300

    Исправленно, что принимает числа в метод.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,33 +5,38 @@ import java.util.Scanner;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
-    static void drawRectangle(int length, int width){
+
+    static void drawRectangle(int length, int width) {
         for (int i = 0; i < length; i++) {
-            if(i==0||i==length-1){
+            if (i == 0 || i == length - 1) {
                 System.out.print(" ");
-            }else{
-              System.out.print("|");}
-                drawLine(length,width, i);
+            } else {
+                System.out.print("|");
+            }
+            drawLine(length, width, i);
             System.out.println();
         }
         scanner.close();
     }
-    static void drawLine(int length,int width,int i){
-        for (int j = 0; j < width-1; j++) {
-            if (j ==width-2) {
-                if (i ==0||i==length-1){
+
+    static void drawLine(int length, int width, int i) {
+        for (int j = 0; j < width - 1; j++) {
+            if (j == width - 2) {
+                if (i == 0 || i == length - 1) {
                     System.out.print(" ");
-                }else{
-                    System.out.print("|");}
-            }else if (i ==0||i==length-1){
+                } else {
+                    System.out.print("|");
+                }
+            } else if (i == 0 || i == length - 1) {
                 System.out.print("-");
-            }else {
+            } else {
                 System.out.print(" ");
             }
         }
     }
+
     public static void main(String[] args) {
         System.out.println("Введите длину и ширину прямоугольника");
-       drawRectangle(scanner.nextInt(), scanner.nextInt());
+        drawRectangle(scanner.nextInt(), scanner.nextInt());
     }
 }
