e3733b564e402cd0f5194cc6034f15293ccf4f7d
Дроздов Алексей <jokend1ng2@yandex.ru>
2023-05-08 22:02:24 +0300

    Исправленно, что принимает числа в метод.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,40 +5,30 @@ import java.util.Scanner;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
-    static int getLength(){
-        System.out.println("Enter length: ");
-        int length = scanner.nextInt();
-        return length;
-    }
-    static int getWidth(){
-        System.out.println("Enter width: ");
-        int width = scanner.nextInt();
-        scanner.close();
-        return width;
-    }
     static void drawRectangle(int length, int width){
-//        int length = getLength();
-//        int width = getWidth();
         for (int i = 0; i < length; i++) {
             if(i==0||i==length-1){
                 System.out.print(" ");
             }else{
               System.out.print("|");}
-            for (int j = 0; j < width-1; j++) {
-               if (j ==width-2) {
-                   if (i ==0||i==length-1){
-                       System.out.print(" ");
-                   }else{
-                    System.out.print("|");}
-                }else if (i ==0||i==length-1){
-                    System.out.print("-");
-                }else {
-                    System.out.print(" ");
-                }
-                }
+                drawLine(length,width, i);
             System.out.println();
         }
         scanner.close();
+    }
+    static void drawLine(int length,int width,int i){
+        for (int j = 0; j < width-1; j++) {
+            if (j ==width-2) {
+                if (i ==0||i==length-1){
+                    System.out.print(" ");
+                }else{
+                    System.out.print("|");}
+            }else if (i ==0||i==length-1){
+                System.out.print("-");
+            }else {
+                System.out.print(" ");
+            }
+        }
     }
     public static void main(String[] args) {
         System.out.println("Введите длину и ширину прямоугольника");
